4aae91a6fd303de4b32f6dc6d3b7598af55a5058
Vivy Cahyani <cahyanivivy@gmail.com>
2021-12-29 11:52:53 +0700

    mengedit template dan layout

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,55 @@
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Ketua | Dashboard</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="{{ asset('template') }}/plugins/googleapis-head.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('template') }}/plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="{{ asset('template') }}/plugins/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="{{ asset('template') }}/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="{{ asset('template') }}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="{{ asset('template') }}/plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('template') }}/dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="{{ asset('template') }}/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="{{ asset('template') }}/plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="{{ asset('template') }}/plugins/summernote/summernote-bs4.min.css">
+
+  <link rel="stylesheet" type="text/css" href="{{ asset('template') }}/datatables/datatables.min.css"/>
+  <script type="text/javascript" src="{{ asset('template') }}/datatables/datatables.min.js"></script>
+    
+  <script src="{{ asset('template') }}/jquery/jquery-3.3.1.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="{{ asset('template') }}/datatables/datatables.min.css"/>
+  <script type="text/javascript" src="{{ asset('template') }}/datatables/datatables.min.js"></script>
+
+  <link rel="stylesheet" type="text/css" href="DataTables/Bootstrap-4-4.1.1/css/bootstrap.min.css"/>
+  <link href="{{ asset('template') }}/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <script src="{{ asset('template') }}/jquery/jquery-3.3.1.min.js"></script>
+  <script src="{{ asset('template') }}/bootstrap/js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="{{ asset('template') }}/dist/js/script.js"></script>
+  <!-- Untuk datatable -->
+  <link rel="stylesheet" type="text/css" href="{{ asset('template') }}/datatables/datatables.min.css"/>
+  <script type="text/javascript" src="{{ asset('template') }}/datatables/datatables.min.js"></script>
+    
+  <script type="text/javascript">
+  $(document).ready(function() {
+      $('#datatable').DataTable();
+  });
+
+</script>
+
+</head>
