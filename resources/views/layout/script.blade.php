f11d8c9315c322588b125fa350c3aae833c7493b
Vivy Cahyani <cahyanivivy@gmail.com>
2021-12-29 11:41:49 +0700

    mmebuat layouts

---
+++ resources/views/layout/script.blade.php
@@ -0,0 +1,66 @@
+
+<!-- jQuery -->
+<script src="{{ asset('template') }}/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{ asset('template') }}/plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('template') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="{{ asset('template') }}/plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="{{ asset('template') }}/plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="{{ asset('template') }}/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="{{ asset('template') }}/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{ asset('template') }}/plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="{{ asset('template') }}/plugins/moment/moment.min.js"></script>
+<script src="{{ asset('template') }}/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="{{ asset('template') }}/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="{{ asset('template') }}/plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="{{ asset('template') }}/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('template') }}/dist/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{ asset('template') }}/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{ asset('template') }}/dist/js/pages/dashboard.js"></script>
+
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": true,
+      "searching": true,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": false,
+    });
+  });
+</script>
+
+<!-- script data tables -->
+ 
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap4.min.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function() {
+    $('#example').DataTable();
+    } );
+  </script>
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,153 @@
+<!-- Sidebar Menu -->
+<nav class="mt-2">
+  <ul class="nav nav-pills nav-sidebar flex-column mt-3" data-widget="treeview" role="menu" data-accordion="false">
+    <!-- Add icons to the links using the .nav-icon class
+       with font-awesome or any other icon font library -->
+
+    <li class="nav-item ">
+      <a href="../dashboard/index.php" class="nav-link active">
+        <i class="nav-icon fas fa-tachometer-alt"></i>
+        <p>
+          Dashboard
+          <i class=""></i>
+        </p>
+      </a>
+    </li>
+    <li class="nav-item ">
+      <a href="#" class="nav-link <br />
+<b>Notice</b>:  Undefined variable: dataUser in <b>C:\xampp\htdocs\audit-keu\sistem\template\sidebar_kepala_spi.php</b> on line <b>220</b><br />
+">
+        <i class="nav-icon fas fa-user"></i>
+        <p>
+          Data User
+          <i class="right fas fa-angle-left"></i>
+        </p>
+      </a>
+      <ul class="nav nav-treeview">
+        <li class="nav-item">
+          <a href="../user/index.php" class="nav-link root">
+            <i class="far fa-circle nav-icon"></i>
+            <p>User</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="../konfirmasi_user/index.php" class="nav-link <br />
+<b>Notice</b>:  Undefined variable: konfirmasi_user in <b>C:\xampp\htdocs\audit-keu\sistem\template\sidebar_kepala_spi.php</b> on line <b>235</b><br />
+">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Konfirmasi User</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="../auditee/index.php" class="nav-link <br />
+<b>Notice</b>:  Undefined variable: auditee in <b>C:\xampp\htdocs\audit-keu\sistem\template\sidebar_kepala_spi.php</b> on line <b>241</b><br />
+">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Auditee</p>
+          </a>
+        </li>
+      </ul>
+    </li>
+
+    <li class="nav-item ">
+      <a href="#" class="nav-link <br />
+<b>Notice</b>:  Undefined variable: dataPka in <b>C:\xampp\htdocs\audit-keu\sistem\template\sidebar_kepala_spi.php</b> on line <b>250</b><br />
+">
+        <i class="nav-icon far fa-calendar-alt"></i>
+        <p>
+          Data PKA
+          <i class="right fas fa-angle-left"></i>
+        </p>
+      </a>
+      <ul class="nav nav-treeview">
+        <li class="nav-item">
+          <a href="../pka/index.php" class="nav-link <br />
+<b>Notice</b>:  Undefined variable: pka in <b>C:\xampp\htdocs\audit-keu\sistem\template\sidebar_kepala_spi.php</b> on line <b>259</b><br />
+">
+            <i class="far fa-circle nav-icon"></i>
+            <p>PKA</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="../pka/timeline.php" class="nav-link <br />
+<b>Notice</b>:  Undefined variable: timeline in <b>C:\xampp\htdocs\audit-keu\sistem\template\sidebar_kepala_spi.php</b> on line <b>265</b><br />
+">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Timeline Audit</p>
+          </a>
+        </li>
+      </ul>
+    </li>
+
+    <li class="nav-item ">
+      <a href="#" class="nav-link <br />
+<b>Notice</b>:  Undefined variable: dataAudit in <b>C:\xampp\htdocs\audit-keu\sistem\template\sidebar_kepala_spi.php</b> on line <b>274</b><br />
+">
+        <i class="nav-icon fas fa-list-alt"></i>
+        <p>
+          Data Audit
+          <i class="right fas fa-angle-left"></i>
+        </p>
+      </a>
+      <ul class="nav nav-treeview">
+        <li class="nav-item">
+          <a href="../desk/index.php" class="nav-link <br />
+<b>Notice</b>:  Undefined variable: desk in <b>C:\xampp\htdocs\audit-keu\sistem\template\sidebar_kepala_spi.php</b> on line <b>283</b><br />
+">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Data Desk</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="../visit/index.php" class="nav-link <br />
+<b>Notice</b>:  Undefined variable: visit in <b>C:\xampp\htdocs\audit-keu\sistem\template\sidebar_kepala_spi.php</b> on line <b>289</b><br />
+">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Data Visit</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="../tha/index.php" class="nav-link <br />
+<b>Notice</b>:  Undefined variable: tha in <b>C:\xampp\htdocs\audit-keu\sistem\template\sidebar_kepala_spi.php</b> on line <b>295</b><br />
+">
+            <i class="far fa-circle nav-icon"></i>
+            <p>THA</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="../lha/index.php" class="nav-link <br />
+<b>Notice</b>:  Undefined variable: lha in <b>C:\xampp\htdocs\audit-keu\sistem\template\sidebar_kepala_spi.php</b> on line <b>301</b><br />
+">
+            <i class="far fa-circle nav-icon"></i>
+            <p>LHA</p>
+          </a>
+        </li>
+      </ul>
+    </li>
+
+    
+    <li class="nav-item ">
+      <a href="../ba/index.php" class="nav-link <br />
+<b>Notice</b>:  Undefined variable: ba in <b>C:\xampp\htdocs\audit-keu\sistem\template\sidebar_kepala_spi.php</b> on line <b>311</b><br />
+">
+        <i class="nav-icon far fa-flag"></i>
+        <p>
+          Berita Acara
+          <i class=""></i>
+        </p>
+      </a>
+    </li>
+    <li class="nav-item ">
+      <a href="{{ asset('template') }}/logout.php" class="nav-link">
+        <i class="nav-icon fas fa-sign-out-alt"></i>
+        <p>
+          Log Out
+          <i class=""></i>
+        </p>
+      </a>
+    </li>
+
+
+  </ul>
+</nav>
+<!-- /.sidebar-menu -->
